8b4599226a040919f9222361626a9fde6dbff74e
danveral <15654181@qq.com>
2019-04-26 17:16:58 +0800

    good to go 6

--- getplot2.py
+++ getplot2.py
@@ -33,8 +33,8 @@ cosVerticalAngle = np.cos(VerticalAnglefor32lasers*np.pi/180.0)
 # [z0,   z1,   z2,   z3,   ... , z384  ],
 # [int0, int1, int2, int3, ... , int384]])
 def processRawData(udpData):
+    
 
-    pass
     return 0
 
 # startFrame should be equal or greater than 1
@@ -77,6 +77,6 @@ def parsePcapGetUdp(filename, startFrame, frameNumber):
     except:
         pass
 
-    return frames
+    return 0
 
 parsePcapGetUdp(sys.argv[1],30,2)
